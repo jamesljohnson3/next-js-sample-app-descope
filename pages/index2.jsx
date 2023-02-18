ab2cde48ff6f6a5fa1da3874b4f48bead9db5252
Admin <support@unlimitpotential.com>
2023-02-18 05:10:02 -0500

    a

--- pages/index2.jsx
+++ pages/index2.jsx
@@ -13,7 +13,7 @@ const App = () => {
 
      if(isAuthenticated){
         return (
-            <><p>Hello ${user.name}</p><button onClick={logout}>Logout</button></>
+            <><p>Hello ${user.email}</p><button onClick={logout}>Logout</button></>
         )
     }
 
