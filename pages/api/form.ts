649b0f7a40b64ba1e1c3f6bc3d13e9a82166c6f0
Admin <support@unlimitpotential.com>
2023-02-18 04:57:39 -0500

    a

--- pages/api/form.ts
+++ pages/api/form.ts
@@ -1,5 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { validateRequestSession } from '../../utils/auth';
+import axios from 'axios';
 
 type Data = {
   data: string
@@ -10,5 +11,16 @@ export default async function handler(
   res: NextApiResponse<Data>
 ) {
   const validated = await validateRequestSession(req);
-  res.status(200).json({ data: validated ? 'Request Validated' : 'Request Not Validated'  })
+  if (validated) {
+    const userData = req.body;
+    const endpoint = 'https://hook.us1.make.com/5gicl1lx7wgxcq7ptcxpb62hh39l57ee';
+    try {
+      await axios.post(endpoint, userData);
+      res.status(200).json({ data: 'Request Validated and User Data Sent' });
+    } catch (error) {
+      res.status(500).json({ data: 'Request Validated but User Data Failed to Send' });
+    }
+  } else {
+    res.status(401).json({ data: 'Request Not Validated' });
+  }
 }
