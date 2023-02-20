1a4eaad02f38d07c18568b508ab0557a2655ea23
Admin <support@unlimitpotential.com>
2023-02-20 18:44:07 -0500

    a

---
+++ pages/radix.tsx
@@ -0,0 +1,10 @@
+import { AppleMusicDemo } from "../components/apple-music-demo"
+export default function IndexPage() {
+    return (
+        <>
+            <AppleMusicDemo />
+      
+        </>
+      )
+    }
+    
