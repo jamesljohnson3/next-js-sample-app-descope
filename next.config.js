4c5800769f5158c8d7d8762726994dc9ebe6db2f
Admin <support@unlimitpotential.com>
2023-02-20 19:29:05 -0500

    a

--- next.config.js
+++ next.config.js
@@ -1,7 +1,27 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ["avatars.githubusercontent.com", "images.unsplash.com", "res.cloudinary.com", "cdn.builder.io"],
+  },
+  experimental: {
+    appDir: true,
+    fontLoaders: [
+      {
+        loader: "@next/font/google",
+        options: { subsets: ["latin"] },
+      },
+    ],
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
 }
 
-module.exports = nextConfig
+module.exports = (nextConfig)
