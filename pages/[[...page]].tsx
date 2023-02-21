35214118e4800a45c1cceccb8769d12861961510
Admin <support@unlimitpotential.com>
2023-02-20 23:22:44 -0500

    a

--- pages/[[...page]].tsx
+++ pages/[[...page]].tsx
@@ -3,7 +3,6 @@ import { useRouter } from 'next/router';
 import { BuilderComponent, Builder, builder } from '@builder.io/react';
 import DefaultErrorPage from 'next/error';
 import Head from 'next/head';
-import { NextSeo } from 'next-seo';
 
 export async function getStaticProps({ params }: GetStaticPropsContext<{ page: string[] }>) {
   const page = await builder
@@ -47,33 +46,7 @@ export default function Page({ page }: InferGetStaticPropsType<typeof getStaticP
     return (
       <>
         <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" /><NextSeo
-      title='NB4F Shop'
-      description='Merchandise, Fashion and more.'
-      openGraph={{
-        url: 'https://nb4fshop.com',
-        title: 'NB4F Shop logo',
-        description:
-          'Merchandise, Fashion and more=',
-        images: [
-          {
-            url: 'https://unlimitednow.live/ogimage.png',
-            height: 600,
-            width: 600,
-            alt: 'nb4fshop',
-          },
-        ],
-      }}
-      twitter={{
-        cardType: 'summary_large_image',
-        handle: '@nb4fshop',
-      }}
-    />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Inter&display=swap"
-            rel="stylesheet"
-          />
-          <link rel="icon" type="image/png" href="/logo.png" />
+       
       </Head>
         <DefaultErrorPage statusCode={404} />
       </>
@@ -83,33 +56,7 @@ export default function Page({ page }: InferGetStaticPropsType<typeof getStaticP
   return (
     <>
              <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" /><NextSeo
-      title='NB4F Shop'
-      description='Merchandise, Fashion and more.'
-      openGraph={{
-        url: 'https://nb4fshop.com',
-        title: 'NB4F Shop logo',
-        description:
-          'Merchandise, Fashion and more=',
-        images: [
-          {
-            url: 'https://unlimitednow.live/ogimage.png',
-            height: 600,
-            width: 600,
-            alt: 'nb4fshop',
-          },
-        ],
-      }}
-      twitter={{
-        cardType: 'summary_large_image',
-        handle: '@nb4fshop',
-      }}
-    />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Inter&display=swap"
-            rel="stylesheet"
-          />
-          <link rel="icon" type="image/png" href="/logo.png" />
+     
       </Head>
       <BuilderComponent model="page" content={page} />
     </>
