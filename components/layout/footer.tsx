86980520990046f36228f398998b3b06d548bc55
Admin <support@unlimitpotential.com>
2023-02-21 17:42:07 -0500

    i

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -20,30 +20,7 @@ export interface FooterProps extends BoxProps {
 export const Footer: React.FC<FooterProps> = (props) => {
   const { columns = 2, ...rest } = props
   return (
-    <Box bg="white" _dark={{ bg: 'gray.900' }} {...rest}>
-      <Container maxW="container.2xl" px="8" py="8">
-        <SimpleGrid columns={columns}>
-          <Stack spacing="8">
-            <Stack alignItems="flex-start">
-              <Flex>
-                <Box as={siteConfig.logo} flex="1" height="32px" />
-              </Flex>
-              <Text fontSize="md" color="muted">
-                {siteConfig.seo.description}
-              </Text>
-            </Stack>
-            <Copyright>{siteConfig.footer.copyright}</Copyright>
-          </Stack>
-          <HStack justify="flex-end" spacing="4" alignSelf="flex-end">
-            {siteConfig.footer?.links?.map(({ href, label }) => (
-              <FooterLink key={href} href={href}>
-                {label}
-              </FooterLink>
-            ))}
-          </HStack>
-        </SimpleGrid>
-      </Container>
-    </Box>
+<></>
   )
 }
 
