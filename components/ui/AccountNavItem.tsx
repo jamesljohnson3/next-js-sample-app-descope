35ff4f975c965f4f14bdfb5adaba6b498bd52b45
Admin <support@unlimitpotential.com>
2023-02-21 19:37:38 -0500

    a

--- components/ui/AccountNavItem.tsx
+++ components/ui/AccountNavItem.tsx
@@ -1,14 +1,18 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import Avatar from '../ui/radix/Avatar';
 import Link from 'next/link';
+import {  useUser } from "@clerk/clerk-react";
 
 import { HiOutlineEllipsisHorizontal } from 'react-icons/hi2';
-
+const { user } = useUser();
+const profileImageUrl = user?.profileImageUrl ?? '';
+  
 const AccountNavItem = () => (
 	<div className="flex flex-1 items-center gap-x-2 px-4 py-8 ">
 		<div className="flex items-center gap-x-3 flex-1">
 			<div className="flex flex-1 xl:flex-none justify-center xl:justify-start">
 				<Avatar
-					src="https://pbs.twimg.com/profile_images/1489998205236527108/q2REh8nW_400x400.jpg"
+					src={profileImageUrl}
 					alt="Roy Quilor"
 					initials="RQ"
 				/>
