c977ed205707c93ec07b819058ce7b78e6dafbec
Admin <support@unlimitpotential.com>
2023-02-21 19:38:53 -0500

    a

--- components/ui/AccountNavItem.tsx
+++ components/ui/AccountNavItem.tsx
@@ -6,20 +6,23 @@ import {  useUser } from "@clerk/clerk-react";
 import { HiOutlineEllipsisHorizontal } from 'react-icons/hi2';
 const { user } = useUser();
 const profileImageUrl = user?.profileImageUrl ?? '';
-  
+const username = user?.username ?? '';
+
+const fullName = user?.fullName ?? '';
+
 const AccountNavItem = () => (
 	<div className="flex flex-1 items-center gap-x-2 px-4 py-8 ">
 		<div className="flex items-center gap-x-3 flex-1">
 			<div className="flex flex-1 xl:flex-none justify-center xl:justify-start">
 				<Avatar
 					src={profileImageUrl}
-					alt="Roy Quilor"
+					alt={username}
 					initials="RQ"
 				/>
 			</div>
 			<div className="hidden xl:flex flex-col ">
-				<p className="text-base font-semibold">Roy Quilor</p>
-				<p className="text-sm text-slate-600 font-medium">@RoyQuilor</p>
+				<p className="text-base font-semibold">{fullName}</p>
+				<p className="text-sm text-slate-600 font-medium">@{username}</p>
 			</div>
 		</div>
 		<div className="hidden xl:flex">
