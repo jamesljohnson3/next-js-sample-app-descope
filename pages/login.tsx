5c72f9e2ca4e1cd7b408e57b4add1e7fae1cf2f7
Admin <support@unlimitpotential.com>
2023-02-23 22:19:57 -0500

    ju

--- pages/login.tsx
+++ pages/login.tsx
@@ -28,7 +28,7 @@ export default function Login() {
       // In production, prefer managing Descope tokens in cookies.
       document.cookie = `DSR=${refreshToken}`;
     }
-    router.push("/index2b");
+    router.push("/saas");
   }, [router]);
 
   const onError = useCallback(
