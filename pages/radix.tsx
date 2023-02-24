04e1a54a0ea05783a48695278a8288ad36c3d1fc
Admin <support@unlimitpotential.com>
2023-02-23 23:40:02 -0500

    a

--- pages/radix.tsx
+++ pages/radix.tsx
@@ -1,8 +1,20 @@
 import { AppleMusicDemo } from "../components/apple-music-demo"
+import {
+	createTheme,
+	NextUIProvider } from "@nextui-org/react";
+  
+  const lightTheme = createTheme({
+	type: "light",
+	theme: {
+	  colors: {}
+	}
+  });
+  
 export default function IndexPage() {
     return (
-        <>
-            <AppleMusicDemo />
+        <><NextUIProvider theme={lightTheme}>
+
+            <AppleMusicDemo /></NextUIProvider> 
       
         </>
       )
