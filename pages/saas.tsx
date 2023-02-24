e0d1842ca7b62acc3a712b9e5324ab8de24c84cd
Admin <support@unlimitpotential.com>
2023-02-24 17:53:07 -0500

    j

--- pages/saas.tsx
+++ pages/saas.tsx
@@ -168,7 +168,6 @@ const HeroSection: React.FC = () => {
           >
             <FallInPlace delay={0.8}>
               <HStack pt="4" pb="12" spacing="8">
-                <NextjsLogo height="28px" /> <ChakraLogo height="20px" />
               </HStack>
 
               <ButtonGroup spacing={4} alignItems="center">
