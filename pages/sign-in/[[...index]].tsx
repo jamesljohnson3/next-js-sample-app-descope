8b650082a94c14c76e695e9853046f2346f8ce60
Admin <support@unlimitpotential.com>
2023-02-25 21:35:19 -0500

    a

--- pages/sign-in/[[...index]].tsx
+++ pages/sign-in/[[...index]].tsx
@@ -1,15 +1,18 @@
-import {SignedOut} from "@clerk/nextjs";
+import {SignedIn, SignedOut} from "@clerk/nextjs";
 import type {NextPage} from "next";
 import {SignInLayout} from "../../layouts/SignInLayout";
 import {SignInForm} from "../../components/SignInForm";
-
+import Box from "../ma";
 const SignIn: NextPage = () => {
   return (
+    <><SignedIn>
+ <Box/>
+    </SignedIn>
     <SignInLayout>
-      <SignedOut>
-        <SignInForm />
-      </SignedOut>
-    </SignInLayout>
+        <SignedOut>
+          <SignInForm />
+        </SignedOut>
+      </SignInLayout></>
   );
 };
 
