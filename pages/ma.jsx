2149fc36c38b89bae1a192ac32e8f5c3f31feea6
Admin <support@unlimitpotential.com>
2023-04-04 07:28:45 -0400

    o

--- pages/ma.jsx
+++ pages/ma.jsx
@@ -19,19 +19,19 @@ import { useForm } from '@mantine/form';
     {
       title: 'Extreme performance',
       description:
-        'This dust is actually a powerful poison that will even make a pro wrestler sick, Regice cloaks itself with frigid air of -328 degrees Fahrenheit',
+        '',
       icon: IconGauge,
     },
     {
       title: 'Privacy focused',
       description:
-        'People say it can run at the same speed as lightning striking, Its icy body is so cold, it will not melt even if it is immersed in magma',
+        '',
       icon: IconUser,
     },
     {
       title: 'No third parties',
       description:
-        'They’re popular, but they’re rare. Trainers who show them off recklessly may be targeted by thieves',
+        '',
       icon: IconCookie,
     },
   ];
