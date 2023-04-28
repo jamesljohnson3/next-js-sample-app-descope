a24dd7b86e8690faf2cf2a34a5f334016a11af27
Admin <support@unlimitpotential.com>
2023-04-28 16:12:05 -0400

    a

--- components/network/index.tsx
+++ components/network/index.tsx
@@ -18,8 +18,7 @@ const Network = () => {
     if (networkOption !== network) {
       setNetwork(networkOption);
       Cookies.set('network', networkOption);
-    } // added closing brace here
-    
+    }
   };
 
   const ActiveNetwork = ({ network }: { network: string }) => {
