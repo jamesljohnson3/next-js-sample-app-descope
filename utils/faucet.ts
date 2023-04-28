aa4a1f2248a2e15742b544ee2d56e828ca18d5de
Admin <support@unlimitpotential.com>
2023-04-28 15:00:31 -0400

    s

--- utils/faucet.ts
+++ utils/faucet.ts
@@ -1,7 +1,8 @@
 import { Networks } from './networks';
+import Cookies from 'js-cookie';
 
 export const getFaucetUrl = () => {
-  const network = localStorage.getItem('network');
+  const network = Cookies.get('network');
   switch (network) {
     case Networks.Polygon:
       return 'https://faucet.polygon.technology/';
