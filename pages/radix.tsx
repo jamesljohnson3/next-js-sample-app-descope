89f5400b889679997705dcb2d7aef478e0678c2a
Admin <support@unlimitpotential.com>
2023-04-28 16:32:33 -0400

    s

--- pages/radix.tsx
+++ pages/radix.tsx
@@ -1,4 +1,4 @@
-import { AppleMusicDemo } from "../components/apple-music-demo"
+import { AppleMusicDemo } from "../components/login"
 import {
 	createTheme,
 	NextUIProvider } from "@nextui-org/react";
