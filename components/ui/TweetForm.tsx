3f479cddca9d6489454047d4c665b0d5c6919357
Admin <support@unlimitpotential.com>
2023-05-21 20:52:39 -0400

    s

--- components/ui/TweetForm.tsx
+++ components/ui/TweetForm.tsx
@@ -44,50 +44,7 @@ const fullName = user?.fullName ?? '';
   return (
     <div className={TweetFormStyles({ width })}>
       <Avatar src={profileImageUrl} alt={fullName} initials="RQ" />
-      <form className="flex flex-col flex-1 gap-y-4">
-        <div className="flex flex-1">
-          <input
-            value={input}
-            onChange={(e) => setInput(e.target.value)}
-            type="text"
-            placeholder="What's up?"
-            className="w-full px-4 py-3 text-xl border-transparent placeholder:text-slate-600 outline-0 focus:outline-none appearance-none focus:ring-0 focus:border-transparent"
-          />
-        </div>
-        <div className="flex justify-between items-center">
-          <div className="flex items-center gap-x-4 px-4">
-            <Link href="https://accounts.unlimitpotential.com/upload">
-              <RiImage2Line className="w-5 h-5" />
-              <span className="sr-only">Image</span>
-            </Link>
-            <Link href="https://accounts.unlimitpotential.com/upload">
-              <RiFileGifLine className="w-5 h-5" />
-              <span className="sr-only">Gif</span>
-            </Link>
-            <Link href="https://lillieai-betav1b.unlimitpotential.com/abilene">
-              <RiChatPollLine className="w-5 h-5" />
-              <span className="sr-only">Poll</span>
-            </Link>
-            <Link href="https://accounts.unlimitpotential.com/upload">
-              <RiEmotionLine className="w-5 h-5" />
-              <span className="sr-only">Emoji</span>
-            </Link>
-            <Link href="https://lillieai-betav1b.unlimitpotential.com/recent">
-              <RiMapPin2Line className="w-5 h-5" />
-              <span className="sr-only">Tag location</span>
-            </Link>
-          </div>
-          <div>
-            <button
-              disabled={!input}
-              onClick={handleBoostClick}
-              className="inline-flex items-center font-bold rounded-full border px-4 py-2 text-sm bg-slate-900 text-white border-transparent disabled:opacity-50 transition-opacity duration-200"
-            >
-              Do more
-            </button>
-          </div>
-        </div>
-      </form>
+    
 
 	  </div>
 	);
