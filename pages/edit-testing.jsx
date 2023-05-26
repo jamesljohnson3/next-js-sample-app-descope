fb8cd285e81655ef6af157e27c63f27d808fc5a9
Admin <support@unlimitpotential.com>
2023-05-26 03:39:29 -0400

    s

--- pages/edit-testing.jsx
+++ pages/edit-testing.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 /* eslint-disable react-hooks/rules-of-hooks */
 import React, { useEffect, useState } from 'react';
 import Uppy from '@uppy/core';
@@ -101,16 +102,92 @@ function UploadForm() {
 
   return (
     <>
-      <SignedIn>
-        Uppy Transloadit Test
-        <DragDrop uppy={uppy} />
-        <form className="needs-validation" onSubmit={handleFormSubmit}>
-          {/* Rest of your form content */}
-        </form>
-        <StatusBar uppy={uppy} />
-      </SignedIn>
-      <SignedOut>Hello</SignedOut>
-    </>
+    Uppy Transloadit Test
+    <DragDrop uppy={uppy} />
+    <form className="needs-validation" onSubmit={handleFormSubmit}>
+        <div className="transloadit-drop-area">
+          <div id="upload-progress-bar"></div>
+        </div>
+
+        <div className="mt-10 border-t border-gray-200 pt-10">
+        <div className="col-span-4">
+                <label htmlFor="name-on-card" className="block text-sm font-medium text-gray-700">
+                  Enter new title
+                </label>
+                <div className="mt-1">
+                  <input
+                    type="text"
+                    id="name"
+                    name="name-on-card"
+                    className="mt-2 block w-full rounded-xl border-2 border-muted-3 bg-transparent px-4 py-2.5 font-semibold text-heading placeholder:text-text/50 focus:border-primary focus:outline-none focus:ring-0 sm:text-sm"
+                    />
+                </div>
+              </div>
+          <RadioGroup value={selectedDeliveryMethod} onChange={setSelectedDeliveryMethod}>
+            <RadioGroup.Label className="text-lg font-medium text-gray-900">Delivery method</RadioGroup.Label>
+
+            <div className="mt-4 grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-4">
+              {deliveryMethods.map((deliveryMethod) => (
+                <RadioGroup.Option
+                  key={deliveryMethod.id}
+                  value={deliveryMethod}
+                  className={({ checked, active }) =>
+                    classNames(
+                      checked ? 'border-transparent' : 'border-gray-300',
+                      active ? 'ring-2 ring-indigo-500' : '',
+                      'relative bg-white border rounded-lg shadow-sm p-4 flex cursor-pointer focus:outline-none'
+                    )
+                  }
+                >
+                  {({ checked, active }) => (
+                    <>
+                      <div className="flex-1 flex">
+                        <div className="flex flex-col">
+                          <RadioGroup.Label as="span" className="block text-sm font-medium text-gray-900">
+                            {deliveryMethod.title}
+                          </RadioGroup.Label>
+                          <RadioGroup.Description as="span" className="mt-1 flex items-center text-sm text-gray-500">
+                            {deliveryMethod.turnaround}
+                          </RadioGroup.Description>
+                          <RadioGroup.Description as="span" className="mt-6 text-sm font-medium text-gray-900">
+                            {deliveryMethod.price}
+                          </RadioGroup.Description>
+                        </div>
+                      </div>
+                      {checked ? <>Icon</> : null}
+                      <div
+                        className={classNames(
+                          active ? 'border' : 'border-2',
+                          checked ? 'border-indigo-500' : 'border-transparent',
+                          'absolute -inset-px rounded-lg pointer-events-none'
+                        )}
+                        aria-hidden="true"
+                      />
+                    </>
+                  )}
+                </RadioGroup.Option>
+              ))}
+            </div>
+          </RadioGroup>
+        </div>
+
+        <div className="photogallery">
+        <div className="photogallery">
+          {uploadedFiles.map((file) => (
+            <img
+              key={file.id}
+              src={file.url}
+              alt={file.name}
+              className="w-full h-auto bg-gray-200 rounded-md"
+            />
+          ))}
+        </div>
+        </div>
+
+        <button type="submit">Upload</button>
+      </form>
+    <StatusBar uppy={uppy} />
+  </>
   );
 }
 
