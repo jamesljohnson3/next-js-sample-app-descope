851f6b0a291c135084db72ffea82615d248b566c
Admin <support@unlimitpotential.com>
2023-05-26 04:01:25 -0400

    m

--- pages/edit-testing.jsx
+++ pages/edit-testing.jsx
@@ -121,6 +121,9 @@ function UploadForm() {
                   <input
                     type="text"
                     id="name"
+                    onChange={(e) =>
+                        setNameOnCard(e.currentTarget.value)
+                    }       
                     name="name-on-card"
                     className="mt-2 block w-full rounded-xl border-2 border-muted-3 bg-transparent px-4 py-2.5 font-semibold text-heading placeholder:text-text/50 focus:border-primary focus:outline-none focus:ring-0 sm:text-sm"
                     />
