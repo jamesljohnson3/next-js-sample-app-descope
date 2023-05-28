2ddaf72e6c1f34979a839f72262a8c93a560f270
Admin <support@unlimitpotential.com>
2023-05-27 23:39:17 -0400

    s

--- components/contracts/index.tsx
+++ components/contracts/index.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
-import Card from '../ui/card';
-import CardHeader from '../ui/card-header';
 import StorageContract from './storage-contract';
 
 export const SmartContracts = () => {
   return (
-    <Card>
-      <CardHeader id="smart-contracts">Smart Contracts</CardHeader>
+    <>
       <StorageContract />
-    </Card>
+    </>
   );
 };
 
