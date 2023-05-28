dc56b9ad2214d7083fb5c8152a765f7aa8f2ec06
Admin <support@unlimitpotential.com>
2023-05-28 01:29:38 -0400

    s

--- components/latests/dashboard/components/team-switcher.tsx
+++ components/latests/dashboard/components/team-switcher.tsx
@@ -58,23 +58,13 @@ const groups = [
   {
     label: "Personal Account",
     teams: [
-      {
-        label: "Alicia Koch",
-        value: "personal",
-      },
+    
     ],
   },
   {
     label: "Teams",
     teams: [
-      {
-        label: "Acme Inc.",
-        value: "acme-inc",
-      },
-      {
-        label: "Monsters Inc.",
-        value: "monsters",
-      },
+      
     ],
   },
 ]
