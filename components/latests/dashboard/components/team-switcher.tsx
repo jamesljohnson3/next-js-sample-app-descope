c274ecdaff47adfcadc603965453e97ab90c58dd
Admin <support@unlimitpotential.com>
2023-05-28 00:51:26 -0400

    j

--- components/latests/dashboard/components/team-switcher.tsx
+++ components/latests/dashboard/components/team-switcher.tsx
@@ -1,132 +1,117 @@
-"use client"
-
+import { ClerkProvider, useUser, SignIn, SignedOut, useClerk } from '@clerk/nextjs'
 import * as React from "react"
 import { Check, ChevronsUpDown, PlusCircle } from "lucide-react"
-
 import { cn } from "../../../../lib/utils"
 import { Avatar, AvatarFallback, AvatarImage } from "../../../../components/ui/avatar"
 import { Button } from "../../../../components/ui/button"
-
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "../../../../components/ui/dialog"
+import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "../../../../components/ui/dialog"
 import { Input } from "../../../../components/ui/input"
 import { Label } from "../../../../components/ui/label"
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "../../../../components/ui/popover"
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "../../../../components/ui/select"
-import { ClerkProvider, useUser, SignIn, SignedOut, useClerk } from '@clerk/nextjs'
+import { Popover, PopoverContent, PopoverTrigger } from "../../../../components/ui/popover"
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "../../../../components/ui/select"
 
 interface PostData {
-index: number;
-id: string;
-featuredImage: string;
-title: string;
-desc: string;
-date: string;
-href: string;
-commentCount: number;
-viewedCount: number;
-readingTime: number;
-bookmark: { count: number; isBookmarked: boolean };
-like: { count: number; isLiked: boolean };
-authorId: number;
-categoriesId: number[];
-postType: string;
-galleryImgs: string[];
-company: string[];
-country: string[];
+  index: number;
+  id: string;
+  featuredImage: string;
+  title: string;
+  desc: string;
+  date: string;
+  href: string;
+  commentCount: number;
+  viewedCount: number;
+  readingTime: number;
+  bookmark: { count: number; isBookmarked: boolean };
+  like: { count: number; isLiked: boolean };
+  authorId: number;
+  categoriesId: number[];
+  postType: string;
+  galleryImgs: string[];
+  company: string[];
+  country: string[];
 }
 
 const groups = [
-{
-label: "Personal Account",
-teams: [
-{
-label: "Alicia Koch",
-value: "personal",
-},
-],
-},
-{
-label: "Teams",
-teams: [
-{
-label: "Acme Inc.",
-value: "acme-inc",
-},
-{
-label: "Monsters Inc.",
-value: "monsters",
-},
-],
-},
+  {
+    label: "Personal Account",
+    teams: [
+      {
+        label: "Alicia Koch",
+        value: "personal",
+      },
+    ],
+  },
+  {
+    label: "Teams",
+    teams: [
+      {
+        label: "Acme Inc.",
+        value: "acme-inc",
+      },
+      {
+        label: "Monsters Inc.",
+        value: "monsters",
+      },
+    ],
+  },
 ]
 
 type Team = (typeof groups)[number]["teams"][number]
 
 type PopoverTriggerProps = React.ComponentPropsWithoutRef<typeof PopoverTrigger>
 
-interface TeamSwitcherProps extends PopoverTriggerProps {}
+interface TeamSwitcherProps extends PopoverTriggerProps {
+  className?: string;
+}
 
 export default function TeamSwitcher({ className }: TeamSwitcherProps) {
-const [open, setOpen] = React.useState(false)
-const [showNewTeamDialog, setShowNewTeamDialog] = React.useState(false)
-const [selectedTeam, setSelectedTeam] = React.useState<Team>(
-groups[0].teams[0]
-)
-const { isLoaded, isSignedIn, user } = useUser()
+  const [open, setOpen] = React.useState(false)
+  const [showNewTeamDialog, setShowNewTeamDialog] = React.useState(false)
+  const [selectedTeam, setSelectedTeam] = React.useState<Team>(
+    groups[0].teams[0]
+  )
+  const { isLoaded, isSignedIn, user } = useUser()
 
-if (!isLoaded || !isSignedIn) {
-return null
-}
+  if (!isLoaded || !isSignedIn) {
+    return null
+  }
 
-let postData: PostData | null = null;
-if (user && user.unsafeMetadata) {
-postData = user.unsafeMetadata as unknown as PostData;
-}
+  const { isLoaded: clerkLoaded, user: clerkUser } = useClerk();
 
-const [selectedArray, setSelectedArray] = React.useState<PostData | null>(null);
-const [selectedImage, setSelectedImage] = React.useState<string | null>(null);
-const [tableTitle, setTableTitle] = React.useState('');
+  if (!clerkLoaded || !clerkUser) {
+    return null;
+  }
 
-React.useEffect(() => {
-if (selectedArray && selectedArray.galleryImgs.length > 0 && !selectedImage) {
-setSelectedImage(selectedArray.galleryImgs[0]);
-}
-}, [selectedArray, selectedImage]);
+  const postData: PostData | null = clerkUser.unsafeMetadata as unknown as PostData | null;
 
-const handleArraySelect = (array: PostData) => {
-setSelectedArray(array);
-setSelectedImage(null);
-setTableTitle(array.title); // Update the table title when a new array is selected
-};
+  const [selectedArray, setSelectedArray] = React.useState<PostData | null>(null);
+  const [selectedImage, setSelectedImage] = React.useState<string | null>(null);
+  const [tableTitle, setTableTitle] = React.useState('');
 
-const handleRowClick = (row: string) => {
-setSelectedImage(row);
-};
+  React.useEffect(() => {
+    if (selectedArray && selectedArray.galleryImgs.length > 0 && !selectedImage) {
+      setSelectedImage(selectedArray.galleryImgs[0]);
+    }
+  }, [selectedArray, selectedImage]);
 
-const findRowById = (id: string) => {
-const selectedRow = selectedArray ? selectedArray.galleryImgs.find((row) => row.id === id) : null;
-return (
-  <>
-    {postData ? (
-      <>
+  const handleArraySelect = (array: PostData) => {
+    setSelectedArray(array);
+    setSelectedImage(null);
+    setTableTitle(array.title); // Update the table title when a new array is selected
+  };
+
+  const handleRowClick = (row: string) => {
+    setSelectedImage(row);
+  };
+
+  const findRowById = (id: string) => {
+    const selectedRow = selectedArray ? selectedArray.galleryImgs.find((row: string) => row === id) : null;
+    return selectedRow;
+  };
+
+  return (
+    <>
+      {postData ? (
         <p>
           <Dialog open={showNewTeamDialog} onOpenChange={setShowNewTeamDialog}>
             <Popover open={open} onOpenChange={setOpen}>
@@ -150,8 +135,7 @@ return (
                   <ChevronsUpDown className="ml-auto h-4 w-4 shrink-0 opacity-50" />
                 </Button>
               </PopoverTrigger>
-              <PopoverContent className="w-[200px] p-0">
-              </PopoverContent>
+              <PopoverContent className="w-[200px] p-0"></PopoverContent>
             </Popover>
             <DialogContent>
               <DialogHeader>
@@ -199,19 +183,12 @@ return (
             </DialogContent>
           </Dialog>
         </p>
-      </>
-    ) : (
-      <>
-        <p>You are not signed in.</p>
-      </>
-    )}
-  </>
-);
-}
-
-return (
-<>
-{findRowById(postData.id)}
-</>
-);
+      ) : (
+        <>
+          <p>You are not signed in.</p>
+        </>
+      )}
+      {findRowById(postData?.id ?? '')}
+    </>
+  );
 }
