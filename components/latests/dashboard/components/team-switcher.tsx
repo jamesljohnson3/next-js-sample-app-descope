1cb1e22eb9f306062a8b7b10ded9cf6e09e7da08
Admin <support@unlimitpotential.com>
2023-06-08 15:05:56 -0400

    r

--- components/latests/dashboard/components/team-switcher.tsx
+++ components/latests/dashboard/components/team-switcher.tsx
@@ -99,7 +99,14 @@ export default function TeamSwitcher({ className }: TeamSwitcherProps) {
     <>        <Dialog open={showNewTeamDialog} onOpenChange={setShowNewTeamDialog}>
     <Popover open={open} onOpenChange={setOpen}>
       <PopoverTrigger asChild>
-        <Button  ref={`https://connect.unlimitpotential.com/${username}`}
+        <Button   ref={(ref: { current: HTMLElement | null; }) => {
+    if (ref) {
+      // Add your logic here if needed
+      // ...
+      // Assign the ref to the desired element
+      ref.current = document.getElementById(`https://connect.unlimitpotential.com/${username}`);
+    }
+  }}
           variant="outline"
           size="sm"
           role="combobox"
