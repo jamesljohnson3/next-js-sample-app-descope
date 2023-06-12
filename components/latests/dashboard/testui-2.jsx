088217929b12efa7a1250fe0a8b428d7325d18fe
Admin <support@unlimitpotential.com>
2023-06-11 23:54:23 -0400

    f

--- components/latests/dashboard/testui-2.jsx
+++ components/latests/dashboard/testui-2.jsx
@@ -30,7 +30,7 @@ function Greeting() {
           <h2 className="text-2xl font-medium mb-4">No data available</h2>
           <p className="text-gray-500">Please add some posts.</p>
           <a
-            href="https://workspace.unlimitpotential.com/create-space"
+            href="https://connect.unlimitpotential.com/collections"
             className="relative block w-full border-2 border-gray-300 border-dashed rounded-lg p-12 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
           >
             <svg
