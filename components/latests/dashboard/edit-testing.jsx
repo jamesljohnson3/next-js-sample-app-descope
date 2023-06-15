ad097651242b1e3db29ccb760c7da8e2e7fea7a4
Admin <support@unlimitpotential.com>
2023-06-15 05:42:12 -0400

    l

--- components/latests/dashboard/edit-testing.jsx
+++ components/latests/dashboard/edit-testing.jsx
@@ -111,9 +111,10 @@ function UploadForm() {
     <Box2/>   <div                 className="relative p-8 bg-white border border-gray-200 rounded-2xl shadow-sm flex flex-col"
 >$
   </div> 
-    <DragDrop uppy={uppy} />
+  
     <form className="needs-validation" onSubmit={handleFormSubmit}>
         <div className="transloadit-drop-area">
+            <DragDrop uppy={uppy} />
           <div id="upload-progress-bar"></div>
         </div>
 
